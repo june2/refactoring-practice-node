b8326600ad34bc2cd50d6b3717a2d33318a293e0
june4539 <kjune@bithumblive.com>
2022-12-04 16:59:34 +0900

    feat: add solution1

--- src/test/solution/_2_replace_temp_with_query/replace-temp-with-query1.test.ts
+++ src/test/solution/_2_replace_temp_with_query/replace-temp-with-query1.test.ts
@@ -1,8 +1,25 @@
 import test from "node:test";
 import { strict as assert } from "node:assert";
+import { ReplaceTempWithQuery1 } from "../../../main/solution/_2_replace_temp_with_query/replace-temp-with-query1";
 
-test("quantity * price 값이 1000 이하의 경우", () => {  
+test("quantity * price 값이 1000 이하의 경우", () => {
+  // given
+  const quantity = 1;
+  const price = 500;
+  const replaceTempWithQuery1 = new ReplaceTempWithQuery1(quantity, price);
+  // when
+  const result = replaceTempWithQuery1.getPrice();
+  // then
+  assert.strictEqual(result, 490);
 });
 
-test("quantity * price 값이 1000 초과일 때", () => {  
+test("quantity * price 값이 1000 초과일 때", () => {
+  // given
+  const quantity = 5;
+  const price = 500;
+  const replaceTempWithQuery1 = new ReplaceTempWithQuery1(quantity, price);
+  // when
+  const result = replaceTempWithQuery1.getPrice();
+  // then
+  assert.strictEqual(result, 2375);
 });
