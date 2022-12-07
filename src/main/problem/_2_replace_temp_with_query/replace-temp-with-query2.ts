1d37ac2d2f9840fc0665ffd9a7d412b8f035c553
june2 <kimjy4536@hotmail.com>
2022-12-07 11:39:46 +0900

    chore: update comment; ;

--- src/main/problem/_2_replace_temp_with_query/replace-temp-with-query2.ts
+++ src/main/problem/_2_replace_temp_with_query/replace-temp-with-query2.ts
@@ -8,10 +8,7 @@ export class ReplaceTempWithQuery2 {
   }
 
   //TODO : 임시변수에 값이 여러번 대입되는 케이스
-  // 1. getPrice메소드에 대해 테스트 케이스 작성
-  // 2. 임시변수에 값이 여러 번 대입되는 경우 Split Temporary Variable을 먼저 시도한다.
-  // 3. Split Temporary Variable(변수쪼개기)는 또다른 임시변수를 선언하는 것
-  // 4. 임시변수에 값이 여러번 대입되면 코드를 읽는 이에게 커다른 혼란을 주기 때문에 Split Temporary Variable를 시도함
+  //메소드설명 : 임시변수에 값이 두 번 할당되는 케이스를 만든 것
   getPrice() {
     let basePrice = this.quantity * this.itemPrice;
 
