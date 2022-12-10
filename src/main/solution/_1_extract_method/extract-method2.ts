b630c4da3f2b83a2b8e1624c2e96255e8762fd25
june2 <kimjy4536@hotmail.com>
2022-12-10 11:06:53 +0900

    feat: add solution

--- src/main/solution/_1_extract_method/extract-method2.ts
+++ src/main/solution/_1_extract_method/extract-method2.ts
@@ -1,25 +1,29 @@
 export class ExtractMethod2 {
-
-    //TODO: 아래의 isPattern을 Extract Method를 이용하여 의미있는 메소드로 만들고, 전체 길이를 줄여보세요.
-    //소스출처 : https://github.com/spring-projects/spring-framework/blob/main/spring-core/src/main/java/org/springframework/util/AntPathMatcher.java
-    //메소드설명 : 빌드툴 Ant 코드 중 경로패턴에 맞는지 체크하는 메소드
+    
     public isPattern(path: string): boolean {
-        let uriVar: boolean = false;
+        let bracketOpened: boolean = false;
 
         for (let i = 0; i < path.length; i++) {
-            let c = path.charAt(i);
-            if (c == '*' || c == '?') {
-                return true;
-            }
-            if (c == '{') {
-                uriVar = true;
-                continue;
-            }
-            if (c == '}' && uriVar) {
+            bracketOpened = this.isBracketOpen(path[i], bracketOpened);
+            if (this.containsAsteriskOrQuestionMark(path[i]) || this.isBracketClosed(path[i], bracketOpened)) {
                 return true;
             }
         }
-
         return false;
+    }
+
+    private isBracketOpen(character: string, isBracketOpen: boolean) {
+        if (character == '{') {
+            return true;
+        }
+        return isBracketOpen;
+    }
+
+    private isBracketClosed(character: string, isBracketOpen: boolean) {
+        return character == '}' && isBracketOpen;
+    }
+
+    private containsAsteriskOrQuestionMark(character: string,) {
+        return ['*', '?'].includes(character);
     }
 }
